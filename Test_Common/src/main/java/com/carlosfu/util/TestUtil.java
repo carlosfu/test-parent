36e372f991ac62e633d8a86b5a81ed988f4397e2
carlosfu <carlosfu@163.com>
2014-09-29 11:15:40 +0800

    pom统一版本，并加入description

---
+++ Test_Common/src/main/java/com/carlosfu/util/TestUtil.java
@@ -0,0 +1,14 @@
+package com.carlosfu.util;
+
+/**
+ * 在Test_Java和Test_Common同时引入com.carlosfu.util.TestUtil
+ * 来测试如果一个工程包含了完全相同class路径的多个类，会出现怎么样的效果
+ * 
+ * @author leifu
+ * @Time 2014年9月29日
+ */
+public class TestUtil {
+    public static void hello() {
+        System.out.println("Test_Common says hello world");
+    }
+}
