b224121da30f8ea9d06f099eca095e9c55e94048
carlosfu <carlosfu@163.com>
2014-10-30 08:46:57 +0800

    jedis官方版本测试

---
+++ Test_Redis_Officials/src/main/java/com/carlosfu/jedis/first/HelloRedis.java
@@ -0,0 +1,20 @@
+package com.carlosfu.jedis.first;
+
+import redis.clients.jedis.Jedis;
+
+/**
+ * 第一个简单的jedis测试
+ * @author leifu
+ * @Date 2014-10-30
+ * @Time 上午8:46:26
+ */
+public class HelloRedis {
+	public static void main(String[] args) {
+		Jedis jedis = new Jedis("10.16.15.86");
+		String key = "hello";
+		jedis.set("hello", "redisWorld");
+		String value = jedis.get(key);
+		System.out.println("value is: " + value);
+		jedis.close();
+	}
+}
