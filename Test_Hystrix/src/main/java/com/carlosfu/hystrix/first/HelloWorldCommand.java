379039540675656cf6c632e98347b9a754fd8571
carlosfu <carlosfu@163.com>
2014-11-29 10:02:51 +0800

    temp 直接找hystrix官方的example看

--- Test_Hystrix/src/main/java/com/carlosfu/hystrix/first/HelloWorldCommand.java
+++ Test_Hystrix/src/main/java/com/carlosfu/hystrix/first/HelloWorldCommand.java
@@ -1,26 +1,73 @@
 package com.carlosfu.hystrix.first;
 
+import java.util.concurrent.TimeUnit;
+
 import com.netflix.hystrix.HystrixCommand;
 import com.netflix.hystrix.HystrixCommandGroupKey;
 
+/**
+ * 第一个Hystrix命令(有点像线程定义) 可以把HelloWorldCommand看错是一个服务或者工具类
+ * 
+ * @author leifu
+ * @Date 2014年11月28日
+ * @Time 下午3:26:36
+ */
 public class HelloWorldCommand extends HystrixCommand<String> {
-	private final String name;
+    private String name;
 
-	public HelloWorldCommand(String name) {
-		// 最少配置:指定命令组名(CommandGroup)
-		super(HystrixCommandGroupKey.Factory.asKey("ExampleGroup"));
-		this.name = name;
-	}
+    private String groupName;
 
-	@Override
-	protected String run() {
-		// 依赖逻辑封装在run()方法中
-		return "Hello " + name + " thread:" + Thread.currentThread().getName();
-	}
+    /**
+     * 默认方法执行1秒
+     */
+    private int commandCostTime = 0;
 
-	
+    public HelloWorldCommand(String name, String groupName) {
+        // must
+        super(HystrixCommandGroupKey.Factory.asKey(groupName));
+        this.name = name;
+    }
+
+    /**
+     * 命令服务的逻辑
+     */
+    @Override
+    protected String run() throws Exception {
+        System.out.println("HelloWorldCommand要等待" + commandCostTime + "秒钟!!");
+        TimeUnit.SECONDS.sleep(commandCostTime);
+        System.out.println("=========方法执行完成==============");
+        return "Hello " + name + ", thread is " + Thread.currentThread().getName();
+    }
+
+//    @Override
+//    protected String getFallback() {
+//        String defaultValue = "!!!!hehe!!!!";
+//        System.out.println("出现了未知错误,只能返回默认值了：" + defaultValue);
+//        return defaultValue;
+//    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public int getCommandCostTime() {
+        return commandCostTime;
+    }
+
+    public void setCommandCostTime(int commandCostTime) {
+        this.commandCostTime = commandCostTime;
+    }
+
 }
-// 运行结果: run()方法在不同的线程下执行
-// result=Hello Synchronous-hystrix thread:hystrix-HelloWorldGroup-1
-// result=Hello Asynchronous-hystrix thread:hystrix-HelloWorldGroup-2
-// mainThread=main
--- Test_Hystrix/src/test/java/com/carlosfu/test/hystrix/first/HelloWorldCommandTest.java
+++ Test_Hystrix/src/test/java/com/carlosfu/test/hystrix/first/HelloWorldCommandTest.java
@@ -6,45 +6,50 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.carlosfu.hystrix.first.HelloWorldCommand;
 
+/**
+ * 第一个helloWorldCommand测试
+ * 
+ * @author leifu
+ * @Date 2014年11月28日
+ * @Time 下午3:31:31
+ */
 public class HelloWorldCommandTest {
 
-	private Logger logger = LoggerFactory.getLogger(HelloWorldCommandTest.class);
+    @Test
+    public void testSynHystrix() {
+        // 每个Command对象只能调用一次,不可以重复调用,
+        // 重复调用对应异常信息:This instance can only be executed once. Please
+        // instantiate a new instance.
+        HelloWorldCommand helloWorldCommand = new HelloWorldCommand("World-hystrix-syn",
+                "Synchronous-hystrix");
+        helloWorldCommand.setCommandCostTime(0);
+        // 使用execute()同步调用代码,效果等同于:helloWorldCommand.queue().get();
+        String result = helloWorldCommand.execute();
+        System.out.println("============Syn result=" + result + "============");
+    }
 
-	@Test
-	public void testSynHystrix() {
-		// 每个Command对象只能调用一次,不可以重复调用,
-		// 重复调用对应异常信息:This instance can only be executed once. Please
-		// instantiate a new instance.
-		HelloWorldCommand helloWorldCommand = new HelloWorldCommand(
-				"Synchronous-hystrix");
-		// 使用execute()同步调用代码,效果等同于:helloWorldCommand.queue().get();
-		String result = helloWorldCommand.execute();
-		logger.info("Syn result=" + result);
-	}
-
-	@Test
-	public void testAsynHystrix() {
-		HelloWorldCommand helloWorldCommand = new HelloWorldCommand(
-				"Asynchronous-hystrix");
-		// 异步调用,可自由控制获取结果时机,
-		Future<String> future = helloWorldCommand.queue();
-		// get操作不能超过command定义的超时时间,默认:1秒
-		String result = null;
-		try {
-			result = future.get(100, TimeUnit.MILLISECONDS);
-		} catch (InterruptedException e) {
-			logger.error(e.getMessage(), e);
-		} catch (ExecutionException e) {
-			logger.error(e.getMessage(), e);
-		} catch (TimeoutException e) {
-			logger.error(e.getMessage(), e);
-		}
-		logger.info("asyn result=" + result);
-	}
+    @Test
+    public void testAsynHystrix() {
+        HelloWorldCommand helloWorldCommand = new HelloWorldCommand("World-hystrix-asyn",
+                "Asynchronous-hystrix");
+        helloWorldCommand.setCommandCostTime(1);
+        // 异步调用,可自由控制获取结果时机,
+        Future<String> future = helloWorldCommand.queue();
+        // get操作不能超过command定义的超时时间,默认:1秒
+        String result = null;
+        try {
+            result = future.get(10000, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        }
+        System.out.println("============aSyn result=" + result + "============");
+    }
 
 }
